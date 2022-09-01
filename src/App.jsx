3fbac714664e7aedd494a4286552a408187c505c
garthendrich <garthendrich@gmail.com>
2022-09-01 14:12:10 +0800

    Create initial app

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,39 @@
+import { useState } from "react";
+import { Timer, TimerOption } from "./components";
+
+const timerOptions = [
+  { label: "Warmup 1 and 2", minutes: 0, seconds: 50 },
+  { label: "Warmup 3", minutes: 2, seconds: 0 },
+  { label: "Press", minutes: 2, seconds: 45 },
+  { label: "Squat", minutes: 3, seconds: 0 },
+  { label: "Chest-Supported Row", minutes: 2, seconds: 0 },
+  { label: "Lateral Raise", minutes: 0, seconds: 50 },
+  { label: "Single-Leg Calf Raise", minutes: 1, seconds: 15 },
+  { label: "Dumbbell Press", minutes: 2, seconds: 45 },
+  { label: "Deadlift", minutes: 3, seconds: 0 },
+  { label: "Overhead Press & Lateral Pulldown", minutes: 2, seconds: 45 },
+  { label: "Face Pull", minutes: 1, seconds: 45 },
+];
+
 function App() {
-  return <h1 className="text-3xl">test</h1>;
+  const [selectedMinutes, setSelectedMinutes] = useState(5);
+  const [selectedSeconds, setSelectedSeconds] = useState(0);
+
+  return (
+    <div className="items-center flex flex-col w-screen min-h-screen">
+      <div className="sticky bg-white w-full p-8 shadow-lg top-0">
+        <Timer minutes={selectedMinutes} seconds={selectedSeconds} />
+      </div>
+      <div className="flex flex-col gap-3 w-full bg-red p-4">
+        {timerOptions.map((options) => (
+          <TimerOption
+            {...options}
+            {...{ setSelectedMinutes, setSelectedSeconds }}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
